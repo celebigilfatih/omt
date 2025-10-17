4e2774012352bdbe642969b040ff2c810154a40c
celebigilfatih <fatihcelebigil@gmail.com>
2025-10-17 16:46:19 +0300

    Update .gitignore and remove large files from cache

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UFL8cSvMnImZr3DHL5mLaPzFJZ6PT9zdGtobJMS7fDQ=\"\n}"
--- .next/static/chunks/src_app_basvuru_page_tsx_0de6bbfb._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_4db3fcb3._.js",
-  "static/chunks/node_modules_zod_v4_c00cc4c6._.js",
-  "static/chunks/node_modules_2e968274._.js"
-],
-    source: "dynamic"
-});
