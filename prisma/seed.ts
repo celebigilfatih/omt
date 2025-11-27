cd8129172227a0866ea9e7c6646d74e5f8d41888
celebigilfatih <fatihcelebigil@gmail.com>
2025-11-27 13:50:40 +0300

    refactor: remove unused prisma config file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,11 @@
 import { PrismaClient, Stage, AgeGroup } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL
+const pool = new pg.Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   // Create sample teams with ageGroupTeamCounts
